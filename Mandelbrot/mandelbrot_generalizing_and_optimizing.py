8770fb324e0119eff07ee6281a6ee7ccb71d7b52
Evan <evan.carpenter14@gmail.com>
2025-09-22 20:39:46 -0400

    SUCCESS, vectorized and parallelized, with guidance of perplexity and documentation. new code was moved to stable file.

--- Mandelbrot/mandelbrot_generalizing_and_optimizing.py
+++ Mandelbrot/mandelbrot_generalizing_and_optimizing.py
@@ -13,11 +13,12 @@ def get_starting_matrix(x_i:float, y_i:float, box_radius:float=1,
 	Create a complex matrix centered at (x_i, y_i) extending to box_radius in each direction.
 	The matrix will have pixel_density^2 total elements.
 	'''
-	global x_min, x_max, y_min, y_max
+	global window
 	x_min = x_i - box_radius
 	x_max = x_i + box_radius
 	y_min = y_i - box_radius
 	y_max = y_i + box_radius
+	window = [x_min, x_max, y_min, y_max]
 
 	re = np.linspace(x_min, x_max, pixel_density, dtype=np.float64)
 	im = np.linspace(y_min, y_max, pixel_density, dtype=np.float64)
@@ -25,78 +26,66 @@ def get_starting_matrix(x_i:float, y_i:float, box_radius:float=1,
 	starting_matrix = re[np.newaxis,:] + (im[:,np.newaxis] * 1j)
 	return starting_matrix
 
+def escape_time_for_row(args):
+		c_row, N_iterations = args
+		z_row = np.zeros_like(c_row)
+		escape_time_row = np.zeros_like(c_row, dtype=int)
 
-
-def escape_time_for_row(row):
-		z = row
-		print(z)
-		N_iterations = 20
 		for iteration in range(N_iterations):
-			z = np.square(z) + c
-			escape_time_row = np.zeros(row.shape, dtype=int)
-			for item_index, item in enumerate(row):
-					if abs(item) > 2:
-						escape_time_row[item_index] = iteration
+			mask = np.abs(z_row) <= 2
+			z_row[mask] = z_row[mask] ** 2 + c_row[mask]
+
+			# Iteration should be added to the escape matrix indices that:
+			# have corresponding z values that were just squared 
+			# (mask)
+			# are now above 2
+			condition1 = np.abs(z_row) > 2
+			# and have not been written to yet
+			condition2 = escape_time_row == 0
+
+			escape_time_row[(mask) & (condition1) & (condition2)] = iteration
+
 		return escape_time_row
 	
-
-def in_mandelbrot(starting_matrix:np.ndarray, N_iterations:int=20) -> np.ndarray:
+def plot_brot(escape_time_matrix, extent:list=None, cmap:str="inferno", norm=PowerNorm(1), filename:str=f"test.png"):
 	'''
-	Determine the "escape time" for each point in the complex matrix c. 
-	Escape time is how many iterations the point takes to exceed an absolute value of 2.
+	Plotting commands for the Mandelbrot set
 	'''
-	global c
-	escape_time_matrix = np.zeros(starting_matrix.shape, dtype=int)
-
-	z = 0
-	c = starting_matrix
-
-	global setname
-	setname = "mndbt" if type(c) == np.ndarray else "julia"
-	
-	
-	for row_index, row in enumerate(starting_matrix):
-		escape_time_matrix[row_index,:] = Pool(processes=2).map(func=escape_time_for_row, iterable=row)
-			#for item_index, item in enumerate(row):
-			#	if abs(item) > 2:
-			#		escape_time_matrix[row_index, item_index] = iteration
-			#		z[row_index,item_index] = 0 
-			#		#c[row_index,item_index] = 0
-			#		#np.delete(z[row_index], item_index)
-			#		#np.delete(c[row_index], item_index)
-	return escape_time_matrix
-
-def main(resolve:bool):
-	from time import time
-	global N_iterations
-	x_i, y_i = 0, 0
-
-	y_i = -y_i
-	rad = 1.5
-
-	if resolve:
-		N_iterations = 400
-		pixel_density = 700
-	else:
-		N_iterations = 250
-		pixel_density = 100
-	
-	start = time()
-	starting_matrix = get_starting_matrix(x_i,y_i, box_radius=rad, pixel_density=pixel_density)
-	X = in_mandelbrot(starting_matrix, N_iterations=N_iterations)
-
-	print(f"Time elapsed: {time()-start:.2f} sec")
-
-	if resolve:
-		# I don't want to save the image with the axes.
-		plt.axis('off')
-		plt.imsave(f"{Path(__file__).parent}/Saved/{x_i}_{y_i}_rad{rad}_pixden{pixel_density}_Niter{N_iterations}.png", X, cmap="inferno", dpi=300)
-		plt.axis('on')
-
-	plt.imshow(X, interpolation="spline36", extent=[x_min, x_max, y_min, y_max], cmap="inferno", norm=PowerNorm(0.5))
+	plt.figure()
+	plt.imshow(escape_time_matrix, interpolation="spline36", extent=extent, cmap=cmap, norm=norm)
 	plt.gca().set_aspect("equal")
 	plt.tight_layout()
 	plt.show()
+	# I don't want to save the image with the axes.
+	plt.axis('off')
+	plt.imsave(f"{Path(__file__).parent}/Saved/{filename}", escape_time_matrix, cmap=cmap)
+
+def main(resolve:bool):
+	from time import time
+	x_i, y_i = -0.755, 0.18
+
+	y_i = -y_i
+	rad = 0.05
+	
+	N_iterations = 1000
+	pixel_density = 2000
+	
+
+
+	start = time()
+	starting_matrix = get_starting_matrix(x_i, y_i, box_radius=rad, pixel_density=pixel_density)
+	# c is the complex matrix, shape (pixeldensity, pixeldensity)
+	with Pool() as pool:
+		escape_time_matrix_rows = pool.map(
+			escape_time_for_row,
+			[(starting_matrix[i, :], N_iterations) for i in range(starting_matrix.shape[0])]
+		)
+	escape_time_matrix = np.array(escape_time_matrix_rows)
+	print(f"Time elapsed: {time()-start:.2f} sec")
+
+	file_name = f"{x_i}_{y_i}_rad{rad}_pixden{pixel_density}_Niter{N_iterations}"
+	plot_brot(escape_time_matrix=escape_time_matrix, extent=window, filename=file_name)
+	
 
 if __name__ == "__main__":
 	main(resolve=True)
--- Mandelbrot/mandelbrot_plot.py
+++ Mandelbrot/mandelbrot_plot.py
@@ -2,18 +2,19 @@ import numpy as np
 from pathlib import Path	
 import matplotlib.pyplot as plt
 from matplotlib.colors import PowerNorm, LogNorm
-
-def get_c_matrix(x_i:float, y_i:float, box_radius:float=1,
+from multiprocessing import Pool
+def get_starting_matrix(x_i:float, y_i:float, box_radius:float=1,
                  pixel_density:int=512) -> np.ndarray:
 	'''
 	Create a complex matrix centered at (x_i, y_i) extending to box_radius in each direction.
-	The matrix will have pixel_density^2 total elements.
+	The matrix shape is (pixeldensity, pixeldensity).
 	'''
-	global x_min, x_max, y_min, y_max
+	global window
 	x_min = x_i - box_radius
 	x_max = x_i + box_radius
 	y_min = y_i - box_radius
 	y_max = y_i + box_radius
+	window = [x_min, x_max, y_min, y_max]
 
 	re = np.linspace(x_min, x_max, pixel_density, dtype=np.float64)
 	im = np.linspace(y_min, y_max, pixel_density, dtype=np.float64)
@@ -21,55 +22,82 @@ def get_c_matrix(x_i:float, y_i:float, box_radius:float=1,
 	c_matrix = re[np.newaxis,:] + (im[:,np.newaxis] * 1j)
 	return c_matrix
 
+
+def escape_time_for_row(args):
+		c_row, N_iterations = args
+		z_row = np.zeros_like(c_row)
+		escape_time_row = np.zeros_like(c_row, dtype=int)
+
+		for iteration in range(N_iterations):
+			mask = np.abs(z_row) <= 2
+			z_row[mask] = z_row[mask] ** 2 + c_row[mask]
+
+			# Iteration should be added to the escape matrix indices that:
+			# have corresponding z values that were just squared 
+			# (mask)
+			# are now above 2
+			condition1 = np.abs(z_row) > 2
+			# and have not been written to yet
+			condition2 = escape_time_row == 0
+
+			escape_time_row[(mask) & (condition1) & (condition2)] = iteration
+
+		return escape_time_row
+
 def in_mandelbrot(c:np.ndarray, N_iterations:int=20) -> np.ndarray:
-	escape_time_matrix = np.zeros(c.shape, dtype=int)
 	'''
 	Determine the "escape time" for each point in the complex matrix c. 
 	Escape time is how many iterations the point takes to exceed an absolute value of 2.
 	'''
 
-	z = 1j
-	for iteration in range(N_iterations):
-		z = np.square(z) + c
-		for row_index, row in enumerate(z):
-			for item_index, item in enumerate(row):
-				if abs(item) > 2:
-					escape_time_matrix[row_index, item_index] = iteration
-					z[row_index,item_index] = 0 
-					c[row_index,item_index] = 0
-					#np.delete(z[row_index], item_index)
-					#np.delete(c[row_index], item_index)
+	escape_time_matrix = np.zeros_like(c, dtype=int)
+
+	for i in range(c.shape[1]):
+		c_row = c[i]
+
+		args = (c_row, N_iterations)
+		escape_time_matrix[i] = escape_time_for_row(args)
+
 	return escape_time_matrix
 
-def main(resolve:bool):
-	from time import time
-	x_i, y_i = 0, 0
-
-	y_i = -y_i
-	rad = 2
-
-	if resolve:
-		N_iterations = 250
-		pixel_density = 900
-	else:
-		N_iterations = 250
-		pixel_density = 100
-	start = time()
-	c = get_c_matrix(x_i,y_i, box_radius=rad, pixel_density=pixel_density)
-	X = in_mandelbrot(c, N_iterations=N_iterations)
-	print(f"Time elapsed: {time()-start:.2f} sec")
-
-	if resolve:
-		# I don't want to save the image with the axes.
-		plt.axis('off')
-		plt.imsave(f"{Path(__file__).parent}/Saved/mbt_{x_i}_{y_i}_rad{rad}_pixden{pixel_density}_Niter{N_iterations}.png", X, cmap="inferno", dpi=300)
-		plt.axis('on')
-
-	plt.imshow(X, interpolation="spline36", extent=[x_min, x_max, y_min, y_max], cmap="inferno", norm=PowerNorm(0.5))
-
+def plot_brot(escape_time_matrix, extent:list=None, cmap:str="inferno", norm=PowerNorm(1), filename:str=f"test.png"):
+	'''
+	Plotting commands for the Mandelbrot set
+	'''
+	plt.figure()
+	plt.imshow(escape_time_matrix, interpolation="spline36", extent=extent, cmap=cmap, norm=norm)
 	plt.gca().set_aspect("equal")
 	plt.tight_layout()
 	plt.show()
+	# I don't want to save the image with the axes.
+	plt.axis('off')
+	plt.imsave(f"{Path(__file__).parent}/Saved/{filename}", escape_time_matrix, cmap=cmap)
+
+def main(resolve:bool):
+	from time import time
+	x_i, y_i = -0.755, 0.18
+
+	y_i = -y_i
+	rad = 0.5
+	
+	N_iterations = 250
+	pixel_density = 10000
+	
+
+
+	start = time()
+	starting_matrix = get_starting_matrix(x_i, y_i, box_radius=rad, pixel_density=pixel_density)
+	# c is the complex matrix, shape (pixeldensity, pixeldensity)
+	with Pool() as pool:
+		escape_time_matrix_rows = pool.map(
+			escape_time_for_row,
+			[(starting_matrix[i, :], N_iterations) for i in range(starting_matrix.shape[0])]
+		)
+	escape_time_matrix = np.array(escape_time_matrix_rows)
+	print(f"Time elapsed: {time()-start:.2f} sec")
+
+	file_name = f"{x_i}_{y_i}_rad{rad}_pixden{pixel_density}_Niter{N_iterations}.png"
+	plot_brot(escape_time_matrix=escape_time_matrix, extent=window, filename=file_name)
 
 if __name__ == "__main__":
 	
