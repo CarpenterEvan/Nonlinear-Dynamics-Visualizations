81a1b6ab666a29b8b3b466ee868d61c64527d1dc
Evan <evan.carpenter14@gmail.com>
2025-09-27 22:48:03 -0400

    tweaks to parameters

--- Mandelbrot/mandelbrot_plot.py
+++ Mandelbrot/mandelbrot_plot.py
@@ -6,21 +6,27 @@ from multiprocessing import Pool
 from pathlib import Path	
 
 
-def get_starting_matrix(x_i:float, y_i:float, box_radius:float=1,
-                 pixel_density:int=512) -> np.ndarray:
+def get_starting_matrix(x_i:float, 
+						y_i:float, 
+						box_size:float=1,
+                		resolution:tuple=(1920,1080)) -> np.ndarray:
 	'''
-	Create a complex matrix centered at (x_i, y_i) extending to box_radius in each direction.
-	The matrix will have pixel_density^2 total elements.
+	Create a complex matrix centered at (x_i, y_i).
+	The matrix will have resolution[0] * resolution[1] total elements.
+	box_size is the vertical size of the window wrt the complex plane. 
+	The horizontal width of the viewing window is determined by the aspect ratio, which is calculated from resolution. 
 	'''
 	global window
-	x_min = x_i - box_radius
-	x_max = x_i + box_radius
-	y_min = y_i - box_radius
-	y_max = y_i + box_radius
-	window = [x_min, x_max, y_min, y_max]
 
-	re = np.linspace(x_min, x_max, pixel_density, dtype=np.float64)
-	im = np.linspace(y_min, y_max, pixel_density, dtype=np.float64)
+	aspect_ratio = resolution[0]/resolution[1]
+
+	x_min = x_i - box_size * aspect_ratio
+	x_max = x_i + box_size * aspect_ratio
+	y_min = y_i - box_size
+	y_max = y_i + box_size
+	window = [x_min, x_max, y_min, y_max]
+	re = np.linspace(x_min, x_max, resolution[0], dtype=np.float64)
+	im = np.linspace(y_min, y_max, resolution[1], dtype=np.float64)
     
 	starting_matrix = re[np.newaxis,:] + (im[:,np.newaxis] * 1j)
 	return starting_matrix
@@ -45,7 +51,31 @@ def escape_time_for_row(args):
 			escape_time_row[(mask) & (condition1) & (condition2)] = iteration
 
 		return escape_time_row
-	
+def in_mandelbrot(x_i:float, y_i:float, box_size:float=1,
+                 resolution:tuple=(), N_iterations:int=20, usePool:bool=True) -> np.ndarray:
+	'''
+	Determine the "escape time" for each point in the complex matrix c. 
+	Escape time is how many iterations the point takes to exceed an absolute value of 2.
+	'''
+	starting_matrix = get_starting_matrix(x_i=x_i, y_i=y_i, box_size=box_size, resolution=resolution)
+	c = starting_matrix
+	escape_time_matrix = np.zeros_like(c, dtype=int)
+
+	if usePool:
+		with Pool(4) as pool:
+			escape_time_matrix_rows = pool.map(
+				escape_time_for_row,
+				[(starting_matrix[i, :], N_iterations) for i in range(starting_matrix.shape[0])]
+			)
+		escape_time_matrix = np.array(escape_time_matrix_rows)
+	else:
+		for i in range(c.shape[1]):
+			c_row = c[i]
+			args = (c_row, N_iterations)
+			escape_time_matrix[i] = escape_time_for_row(args)
+
+	return escape_time_matrix
+
 def plot_brot(escape_time_matrix, show:bool=True, extent:list=None, cmap:str="inferno", norm=PowerNorm(1), filename:Path=Path("test.png")):
 	'''
 	Plotting commands for the Mandelbrot set
@@ -67,42 +97,18 @@ def plot_brot(escape_time_matrix, show:bool=True, extent:list=None, cmap:str="in
 		plt.imsave(filename, escape_time_matrix, cmap=cmap)
 	plt.close()
 
-def in_mandelbrot(x_i:float, y_i:float, box_radius:float=1,
-                 pixel_density:int=512, N_iterations:int=20, usePool:bool=True) -> np.ndarray:
-	'''
-	Determine the "escape time" for each point in the complex matrix c. 
-	Escape time is how many iterations the point takes to exceed an absolute value of 2.
-	'''
-	starting_matrix = get_starting_matrix(x_i=x_i, y_i=y_i, box_radius=box_radius, pixel_density=pixel_density)
-	c = starting_matrix
-	escape_time_matrix = np.zeros_like(c, dtype=int)
-
-	if usePool:
-		with Pool() as pool:
-			escape_time_matrix_rows = pool.map(
-				escape_time_for_row,
-				[(starting_matrix[i, :], N_iterations) for i in range(starting_matrix.shape[0])]
-			)
-		escape_time_matrix = np.array(escape_time_matrix_rows)
-	else:
-		for i in range(c.shape[1]):
-			c_row = c[i]
-			args = (c_row, N_iterations)
-			escape_time_matrix[i] = escape_time_for_row(args)
-
-	return escape_time_matrix
 
 def main(makeFrames:bool=False):
 	from time import time
 	x_i, y_i = -1.78, 0
 	# -0.8181285, 0.20082240 # near spiral, slightly too high
-	max_N_iterations = 550
-	max_pixel_density = 1_500
-	smallest_radius = 0.0001
-	largest_radius = 2
+	initial_N_iterations = 300#
+	smallest_size = 0.0001
+	largest_size = 2
 
 	fps = 24
-	seconds = 10
+	resolution = (1920, 1080)# 1080p
+	seconds = 20
 	N_frames = int(fps * seconds)
 
 	y_i = -y_i # so moving based on imshow coords is more intuitive
@@ -112,58 +118,61 @@ def main(makeFrames:bool=False):
 	frames = np.arange(start=1,stop=N_frames+1)
 
 
-	# Experimentally paramaterizing the radius, N_iterations, and pixel density from frames
-	# TODO: Zoom for radius should be parameterized as exponential
+	# Experimentally paramaterizing the N_iterations and pixel density from frames
+	# TODO: Zoom for radius should be parameterized as exponential?
 	# https://stackoverflow.com/questions/47818880/
 	
-	radius_values = np.geomspace(smallest_radius, largest_radius, N_frames)
-	if True:
-		import pandas as pd
-		to_copy = pd.DataFrame(radius_values).to_clipboard()
-		exit()
-	pixel_density_values = (max_pixel_density - 2 * frames).astype(int)
-	N_iter_values = (max_N_iterations - 2.5 * frames).astype(int)
+	box_size_values = np.geomspace(smallest_size, largest_size, N_frames)
 
+	N_iter_values = (  ((N_frames-frames)**2)/N_frames + initial_N_iterations ).astype(int)
+	#(max_N_iterations - ((max_N_iterations-50)/ N_frames) * frames).astype(int)
+
+	####################################################################################
 	start = time()
 	if makeFrames:
 		subfolder = Saved / Path(f"frames/single_zoom/experiment/x{x_i}_y{y_i}_geomspace_different_speeds")
 		loopstart = time()
+		try:
+			for frame in frames:
+				index = frame-1
+				frame_label_index = N_frames-index
+				box_size = box_size_values[index]
+				N_iterations = N_iter_values[index]
 
-		for frame in frames:
-			index = frame-1
-			frame_label_index = N_frames-frame
-			radius = radius_values[index]
-			N_iterations = N_iter_values[index]
-			pixel_density = pixel_density_values[index]
-			
-			
-			start = time()
-			escape_time_matrix = in_mandelbrot(x_i=x_i, 
-									  		   y_i=y_i,
-									  		   box_radius=radius, 
-									  		   pixel_density=pixel_density, 
-									  		   N_iterations=N_iterations)
-			
-			print(f"{frame=: >3}/{N_frames: >3}\t{time()-start:.1f} seconds")
+				
+				start = time()
+				escape_time_matrix = in_mandelbrot(x_i=x_i, 
+												y_i=y_i,
+												box_size=box_size, 
+												resolution=resolution, 
+												N_iterations=N_iterations)
+				
+				print(f"{frame=: >3}/{N_frames: >3}\t{round(time()-start,1): >5} seconds",end="\r")
 
-			filename = subfolder / Path(f"frame{frame_label_index:0>4}.png")
-			plot_brot(escape_time_matrix=escape_time_matrix, extent=window, filename=filename, show=False)
-		
-		os.system(f"ffmpeg -r {fps}\
-					-f image2\
-					-s {pixel_density}x{pixel_density}\
-					-pattern_type glob\
-					-i '{subfolder}/*.png'\
-					-vcodec libx264\
-					-crf 18\
-					-pix_fmt yuv420p\
-					{subfolder}/mandelbrot_linear_parameterization.mp4")
-		print(f"Total time elapsed: {time()-loopstart:.1f} seconds")
+				filename = subfolder / Path(f"frame{frame_label_index:0>4}.png")
+				plot_brot(escape_time_matrix=escape_time_matrix, extent=window, filename=filename, show=False)
+			
+			os.system(f"ffmpeg -r {fps}\
+			 			-y\
+						-f image2\
+						-s {resolution[0]}x{resolution[1]}\
+						-pattern_type glob\
+						-i '{subfolder}/*.png'\
+						-vcodec libx264\
+						-crf 18\
+						-pix_fmt yuv420p\
+						{subfolder}/mandelbrot_linear_parameterization.mp4")
+			print("\n")
+			print(f"Total time elapsed: {time()-loopstart:.1f} seconds")
+		except KeyboardInterrupt:
+			print("Exiting...")
 	else: 
+		box_size = box_size_values[0]
+		N_iterations = N_iter_values[0]
 		escape_time_matrix = in_mandelbrot(x_i=x_i, 
 								  		   y_i=y_i,
-								  		   box_radius=smallest_radius, 
-								  		   pixel_density=pixel_density, 
+								  		   box_size=smallest_size, 
+								  		   resolution=resolution, 
 								  		   N_iterations=N_iterations)
 		
 		plot_brot(escape_time_matrix=escape_time_matrix, extent=window, show=True)
